ea1de5d324df42abc975aa05508ab2dcf6c31bbf
Redovarnis <cihanvural_2020@outlook.com>
2022-04-17 18:15:28 +0300

    Laravel Admin Panel Template Implementation

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminPanel\HomeController as AdminHomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -36,3 +37,7 @@ Route::get('/header', [HomeController::class, 'header'])->name('header');
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+
+// ****************** ADMIN PANEL ROUTES **********************
+Route::get('/admin', [AdminHomeController::class, 'index'])->name('admin');
