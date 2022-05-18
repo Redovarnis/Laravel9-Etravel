3b0bb955624c0d52bc8f4519af061e59fdc6ea25
Redovarnis <cihanvural_2020@outlook.com>
2022-05-18 13:16:56 +0300

    Laravel Admin Sidebar Links & Icons
    -Sidebar icons has been placed.

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -7,8 +7,15 @@
 
 			<main class="content">
 				<div class="container-fluid p-0">
-					<h1 class="h3 mb-3">Category List</h1>
-
+                    <div class="btn-group mb-3" role="group" aria-label="Category Modification">
+                        <a href="/admin/category/create">
+                            <button type="button" class="btn btn-primary">
+                                <i class="align-middle" data-feather="plus-square"></i>
+                                Add Category</button>
+                        </a>
+                        <button type="button" class="btn btn-secondary">Middle</button>
+                        <button type="button" class="btn btn-secondary">Right</button>
+                    </div>
                     <div class="row">
                         <div class="col-12">
                             <!-- Cardholder START -->
@@ -33,7 +40,8 @@
                                                                 <th class="d-none d-xl-table-cell">Keywords</th>
                                                                 <th class="d-none d-md-table-cell">Description</th>
                                                                 <th class="d-none d-md-table-cell">Image</th>
-                                                                <th class="d-none d-md-table-cell">Status</th>                                                                <th style="width: 40px" class="d-none d-md-table-cell">Edit</th>
+                                                                <th class="d-none d-md-table-cell">Status</th>
+                                                                <th style="width: 40px" class="d-none d-md-table-cell">Edit</th>
                                                                 <th style="width: 40px" class="d-none d-md-table-cell">Action</th>
                                                                 <th style="width: 40px" class="d-none d-md-table-cell">Show</th>
                                                             </tr>
@@ -47,9 +55,15 @@
                                                                 <td class="d-none d-xl-table-cell">{{$rs->description}}</td>
                                                                 <td class="d-none d-md-table-cell">{{$rs->image}}</td>
                                                                 <td class="d-none d-md-table-cell">{{$rs->status}}</td>
-                                                                <td><a href="/admin/category/edit/{{$rs->id}}" class="btn btn-primary btn-sm">Edit</a></td>
-                                                                <td><a href="/admin/category/delete/{{$rs->id}}" class="btn btn-danger btn-sm">Delete</a></td>
-                                                                <td><a href="/admin/category/show/{{$rs->id}}" class="btn btn-success btn-sm">Show</a></td>
+                                                                <td><a href="/admin/category/edit/{{$rs->id}}" class="btn btn-sm text-none btn-warning" style="text-decoration: none">
+                                                                    <i class="align-middle" data-feather="edit-3"></i>Edit
+                                                                </a></td>
+                                                                <td><a href="/admin/category/delete/{{$rs->id}}" class="btn  btn-sm btn-danger" style="text-decoration: none">
+                                                                    <i class="align-middle" data-feather="trash-2"></i>Delete
+                                                                </a></td>
+                                                                <td><a href="/admin/category/show/{{$rs->id}}" class="btn btn-sm btn-success" style="text-decoration: none">
+                                                                    <i class="align-middle" data-feather="grid"></i>Show
+                                                                </a></td>
                                                             </tr>
                                                         @endforeach
                                                         </tbody>
--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -5,94 +5,90 @@
         <nav id="sidebar" class="sidebar js-sidebar">
 			<div class="sidebar-content js-simplebar">
 				<a class="sidebar-brand" href="http://127.0.0.1:8000/admin">
-                    <span class="align-middle">AdminKit</span>
+                    <span class="align-middle">E-Travel</span>
                 </a>
-
 				<ul class="sidebar-nav">
-					<li class="sidebar-header">
-						Pages
+                    <li class="sidebar-header">Profile</li>
+                    <li class="sidebar-item">
+						<a class="sidebar-link" href="/admin">
+                            <i class="align-middle" data-feather="user"></i>
+                            <span class="align-middle">Dashboard</span>
+                        </a>
+					</li>
+                    <li class="sidebar-header">
+						Posts
 					</li>
 
 					<li class="sidebar-item">
 						<a class="sidebar-link" href="index.html">
-                            <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboard</span>
+                            <i class="align-middle" data-feather="hash"></i>
+                            <span class="align-middle">New Posts</span>
+                        </a>
+					</li>
+                    <li class="sidebar-item">
+						<a class="sidebar-link" href="index.html">
+                            <i class="align-middle text-success" data-feather="check"></i>
+                            <span class="align-middle">Accepted Posts</span>
+                        </a>
+					</li>
+                    <li class="sidebar-item">
+						<a class="sidebar-link" href="index.html">
+                            <i class="align-middle text-warning" data-feather="activity"></i>
+                            <span class="align-middle">Shipping </span>
+                        </a>
+					</li>
+                    <li class="sidebar-item">
+						<a class="sidebar-link" href="index.html">
+                            <i class="align-middle text-success" data-feather="check-circle"></i>
+                            <span class="align-middle">Completed Posts</span>
                         </a>
 					</li>
 
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="pages-profile.html">
-                            <i class="align-middle" data-feather="user"></i> <span class="align-middle">Profile</span>
+                    <li class="sidebar-header"></li>
+                    <li class="sidebar-item">
+						<a class="sidebar-link" href="/admin/category">
+                            <i class="align-middle text-warning" data-feather="list"></i>
+                            <span class="align-middle">Categories</span>
+                        </a>
+					</li>
+                    <li class="sidebar-item">
+						<a class="sidebar-link" href="/admin/post">
+                            <i class="align-middle text-info" data-feather="send"></i>
+                            <span class="align-middle">Posts</span>
+                        </a>
+					</li>
+                    <li class="sidebar-item">
+						<a class="sidebar-link" href="/admin/comment">
+                            <i class="align-middle text-info" data-feather="message-square"></i>
+                            <span class="align-middle">Comments</span>
+                        </a>
+					</li>
+                    <li class="sidebar-item">
+						<a class="sidebar-link" href="/admin/faq">
+                            <i class="align-middle text-warning" data-feather="help-circle"></i>
+                            <span class="align-middle">FAQ</span>
+                        </a>
+					</li>
+                    <li class="sidebar-item">
+						<a class="sidebar-link" href="/admin/user">
+                            <i class="align-middle text-success" data-feather="user"></i>
+                            <span class="align-middle">Users</span>
+                        </a>
+					</li>
+                    <li class="sidebar-item">
+						<a class="sidebar-link" href="/admin/social">
+                            <i class="align-middle text-info" data-feather="message-circle"></i>
+                            <span class="align-middle">Social</span>
                         </a>
 					</li>
 
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="pages-sign-in.html">
-                            <i class="align-middle" data-feather="log-in"></i> <span class="align-middle">Sign In</span>
+                    <li class="sidebar-header">LABELS</li>
+                    <li class="sidebar-item">
+						<a class="sidebar-link" href="/admin/setting">
+                            <i class="align-middle text-secondary" data-feather="settings"></i>
+                            <span class="align-middle">Settings</span>
                         </a>
 					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="pages-sign-up.html">
-                            <i class="align-middle" data-feather="user-plus"></i> <span class="align-middle">Sign Up</span>
-                        </a>
-					</li>
-
-					<li class="sidebar-item active">
-						<a class="sidebar-link" href="pages-blank.html">
-                            <i class="align-middle" data-feather="book"></i> <span class="align-middle">Blank</span>
-                        </a>
-					</li>
-
-					<li class="sidebar-header">
-						Tools & Components
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="ui-buttons.html">
-                            <i class="align-middle" data-feather="square"></i> <span class="align-middle">Buttons</span>
-                        </a>
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="ui-forms.html">
-                            <i class="align-middle" data-feather="check-square"></i> <span class="align-middle">Forms</span>
-                        </a>
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="ui-cards.html">
-                            <i class="align-middle" data-feather="grid"></i> <span class="align-middle">Cards</span>
-                        </a>
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="ui-typography.html">
-                            <i class="align-middle" data-feather="align-left"></i> <span class="align-middle">Typography</span>
-                        </a>
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="icons-feather.html">
-                            <i class="align-middle" data-feather="coffee"></i> <span class="align-middle">Icons</span>
-                        </a>
-					</li>
-
-					<li class="sidebar-header">
-						Plugins & Addons
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="charts-chartjs.html">
-                            <i class="align-middle" data-feather="bar-chart-2"></i> <span class="align-middle">Charts</span>
-                        </a>
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="maps-google.html">
-                            <i class="align-middle" data-feather="map"></i> <span class="align-middle">Maps</span>
-                        </a>
-					</li>
-				</ul>
 
 				<div class="sidebar-cta">
 					<div class="sidebar-cta-content">
